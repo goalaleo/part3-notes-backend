fdb1ff79329fc9811bd4aa6367d972d884162f39
Matti Luukkainen <mluukkai@iki.fi>
2018-01-20 13:25:27 +0200

    stuf for heroku

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(bodyParser.json())
+
+const logger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(logger)
 
 let notes = [
   {
@@ -24,7 +37,6 @@ let notes = [
     important: true
   }
 ]
-
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -76,7 +88,13 @@ app.delete('/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const error = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(error)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
