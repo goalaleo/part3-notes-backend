d73645976327f6cc7f8834bd079da97b349fcdc2
Matti Luukkainen <mluukkai@iki.fi>
2018-01-20 21:11:18 +0200

    db url from env

--- models/note.js
+++ models/note.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://fullstack:fullstack@ds211088.mlab.com:11088/fullstack-notes'
+//const url = 'mongodb://fullstack:fullstack@ds211088.mlab.com:11088/fullstack-notes'
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 mongoose.Promise = global.Promise
