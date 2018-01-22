16834c8d59e82b67c28ba048be74c40a8f2e3fc4
Matti Luukkainen <mluukkai@iki.fi>
2018-01-22 20:15:21 +0200

    test env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,13 +6,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
+const config = require('./utils/config')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
 app.use(cors())
@@ -23,7 +20,16 @@ app.use('/api/notes', notesRouter)
 
 app.use(middleware.error)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
